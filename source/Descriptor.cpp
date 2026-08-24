b8ec5fce38cfd62fa1d2defaa309cd2666ef0d64
Julian Bustillos <bustillosjulian@gmail.com>
2026-08-25 00:27:59 +0200

    Improve descriptor distance

--- source/Descriptor.cpp
+++ source/Descriptor.cpp
@@ -9,5 +9,10 @@ void Descriptor::compute(const cv::Mat& image)
 }
 
 double Descriptor::distance(const Descriptor& other) const {
-    return level4.meanDistance(other.level4);
+    return 0.2  * level1.meanDistance(other.level1) + 
+           0.3  * level2.meanDistance(other.level2) +
+           0.5  * level4.meanDistance(other.level4) + 
+           0.1  * level1.stdDistance(other.level1) +
+           0.1  * level2.stdDistance(other.level2) + 
+           0.05 * level4.stdDistance(other.level4);
 }
